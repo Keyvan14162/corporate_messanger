fb02ed09810504680247efb2c7ffa892292b4c2c
ismail_kyvsn <ismailkyvsn2000@gmail.com>
2022-09-09 16:30:56 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_firebase_auth/route_generator.dart';
 import 'firebase_options.dart';
 
@@ -12,7 +13,7 @@ void main() async {
   );
 
   // hide android bottom buttons
-  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 
   runApp(const MyApp());
 }
